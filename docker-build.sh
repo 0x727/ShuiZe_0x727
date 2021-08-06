a9861d760d69dc1c49adf79c19188bf7f666a957
ske <ske@github.com>
2021-08-06 13:37:32 +0800

    update

--- docker-build.sh
+++
@@ -1,9 +0,0 @@
-apt-get update
-apt install git --fix-missing -y
-apt install vim -y
-apt install tmux -y
-rm /usr/bin/python3
-ln -s /usr/local/bin/python3.8 /usr/bin/python3
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-chmod 777 ./Plugins/infoGather/subdomain/ksubdomain/ksubdomain_linux
